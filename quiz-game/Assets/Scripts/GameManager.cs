dfccc3c3589c2a3e5b2d7cc6fd39ae1c8cf925e7
Rato Kuzmanić <rato.kuzmanic@live.com>
2018-05-23 00:09:05 +0200

    Start working on project

--- quiz-game/Assets/Scripts/GameManager.cs
+++ quiz-game/Assets/Scripts/GameManager.cs
@@ -7,75 +7,10 @@ using System.Linq;
 
 public class GameManager : MonoBehaviour {
 	public Question[] questions;
-	private static int score = 0;
-
-	private IList<Question> unanswered;
-	private Question currentQuestion;
-
-	private float delayTime = 1.5f;
-	IEnumerator DisplayNextQuestion() {
-		yield return new WaitForSeconds(delayTime);
-		//Application.LoadLevel("scene");
-		trueButton.SetActive(true);
-		falseButton.SetActive(true);
-		answerText.text = "";
-		SetCurrentQuestion();
-	}
-
-	[SerializeField]
-	private Text questionText;
-
-	[SerializeField]
-	private Text answerText;
-
-	[SerializeField]
-	private Text scoreText;
-
-	[SerializeField]
-	private GameObject trueButton;
-
-	[SerializeField]
-	private GameObject falseButton;
-
-	public void UserSelect(bool answer) {
-		trueButton.SetActive(false);
-		falseButton.SetActive(false);
-
-		if(answer == currentQuestion.isTrue) {
-			answerText.text = "Yeeep!";
-			answerText.color = new Color(29 / 255f, 174 / 255f, 96 / 255f);
-			score += 1;
-		}
-		else {
-			answerText.text = "Nop :(";
-			answerText.color = new Color(216 / 255f, 27 / 255f, 70 / 255f);
-		}
-
-		scoreText.text = "Bodovi: " + score;
-
-		StartCoroutine("DisplayNextQuestion");
-	}
-
-	void SetCurrentQuestion() {
-		int index = UnityEngine.Random.Range(0, unanswered.Count);
-
-		currentQuestion = unanswered[index];
-		questionText.text = currentQuestion.statement;
-
-		unanswered.Remove(currentQuestion);
-	}
 	
-	void Start () {
-		unanswered = new List<Question>();
-		if (!unanswered.Any ()) {
-			unanswered = questions.ToList();
-		}
-
-		trueButton.SetActive(true);
-		falseButton.SetActive(true);
-		answerText.text = "";
-
-		SetCurrentQuestion();
+	void Start ()
+	{
+	    questions = questions.Concat(Question.Seed()).ToArray();
 	}
 
 	void Update () {
--- quiz-game/Assets/Scripts/Question.cs
+++ quiz-game/Assets/Scripts/Question.cs
@@ -1,7 +1,44 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Choice
+{
+    public string Statement;
+    public bool IsTrue;
+
+    public Choice(string statement)
+    {
+        Statement = statement;
+        IsTrue = false;
+    }
+}
 
 [Serializable]
 public class Question {
-	public string statement;
-	public bool isTrue;
+	public string Text;
+
+    [SerializeField]
+	public Choice[] Choices;
+
+    public Question(string text, string[] choices, int correctChoiceLocation)
+    {
+        Text = text;
+        Choices = new Choice[choices.Length];
+        for (var i = 0; i < choices.Length; i++)
+        {
+            Choices[i] = new Choice(choices[i]);
+        }
+        Choices[correctChoiceLocation - 1].IsTrue = true;
+    }
+
+    public static Question[] Seed()
+    {
+        return new []
+        {
+            new Question("What is the capital city of Croatia", new [] { "Split", "Zagreb", "Rijeka", "Osijek" }, 2)
+        };
+    }
 }
